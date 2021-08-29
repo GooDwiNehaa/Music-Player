6a9e52f763881b1542354c75acf763c51518cfe4
GooDwiNehaa <GooDw1Nehaaa@yandex.ru>
2021-08-29 21:11:09 +0300

    Add project files.

---
+++ Music Player/MusicPlayer.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Music_Player
+{
+    partial class MusicPlayer
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PlayListBox = new System.Windows.Forms.ListBox();
+            this.Play = new System.Windows.Forms.Button();
+            this.Pause = new System.Windows.Forms.Button();
+            this.Continue = new System.Windows.Forms.Button();
+            this.Previo = new System.Windows.Forms.Button();
+            this.Next = new System.Windows.Forms.Button();
+            this.TrackName = new System.Windows.Forms.Label();
+            this.TrackBar = new System.Windows.Forms.TrackBar();
+            this.CurrentTime = new System.Windows.Forms.Label();
+            this.FullTime = new System.Windows.Forms.Label();
+            this.Add = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.TrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PlayListBox
+            // 
+            this.PlayListBox.FormattingEnabled = true;
+            this.PlayListBox.ItemHeight = 16;
+            this.PlayListBox.Location = new System.Drawing.Point(123, 12);
+            this.PlayListBox.Name = "PlayListBox";
+            this.PlayListBox.Size = new System.Drawing.Size(295, 308);
+            this.PlayListBox.TabIndex = 0;
+            this.PlayListBox.SelectedIndexChanged += new System.EventHandler(this.PlayListBox_SelectedIndexChanged);
+            this.PlayListBox.DoubleClick += new System.EventHandler(this.PlayListBox_DoubleClick);
+            // 
+            // Play
+            // 
+            this.Play.Enabled = false;
+            this.Play.Location = new System.Drawing.Point(18, 12);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(86, 29);
+            this.Play.TabIndex = 1;
+            this.Play.Text = "Play";
+            this.Play.UseVisualStyleBackColor = true;
+            this.Play.Click += new System.EventHandler(this.Play_Click);
+            // 
+            // Pause
+            // 
+            this.Pause.Location = new System.Drawing.Point(18, 72);
+            this.Pause.Name = "Pause";
+            this.Pause.Size = new System.Drawing.Size(86, 29);
+            this.Pause.TabIndex = 2;
+            this.Pause.Text = "Pause";
+            this.Pause.UseVisualStyleBackColor = true;
+            this.Pause.Click += new System.EventHandler(this.Pause_Click);
+            // 
+            // Continue
+            // 
+            this.Continue.Enabled = false;
+            this.Continue.Location = new System.Drawing.Point(18, 127);
+            this.Continue.Name = "Continue";
+            this.Continue.Size = new System.Drawing.Size(86, 29);
+            this.Continue.TabIndex = 3;
+            this.Continue.Text = "Continue";
+            this.Continue.UseVisualStyleBackColor = true;
+            this.Continue.Click += new System.EventHandler(this.Continue_Click);
+            // 
+            // Previo
+            // 
+            this.Previo.Location = new System.Drawing.Point(18, 232);
+            this.Previo.Name = "Previo";
+            this.Previo.Size = new System.Drawing.Size(86, 29);
+            this.Previo.TabIndex = 4;
+            this.Previo.Text = "Previo";
+            this.Previo.UseVisualStyleBackColor = true;
+            this.Previo.Click += new System.EventHandler(this.Previo_Click);
+            // 
+            // Next
+            // 
+            this.Next.Location = new System.Drawing.Point(18, 179);
+            this.Next.Name = "Next";
+            this.Next.Size = new System.Drawing.Size(86, 29);
+            this.Next.TabIndex = 5;
+            this.Next.Text = "Next";
+            this.Next.UseVisualStyleBackColor = true;
+            this.Next.Click += new System.EventHandler(this.Next_Click);
+            // 
+            // TrackName
+            // 
+            this.TrackName.Location = new System.Drawing.Point(15, 335);
+            this.TrackName.Name = "TrackName";
+            this.TrackName.Size = new System.Drawing.Size(472, 17);
+            this.TrackName.TabIndex = 6;
+            // 
+            // TrackBar
+            // 
+            this.TrackBar.Enabled = false;
+            this.TrackBar.Location = new System.Drawing.Point(8, 355);
+            this.TrackBar.Name = "TrackBar";
+            this.TrackBar.Size = new System.Drawing.Size(479, 56);
+            this.TrackBar.TabIndex = 7;
+            this.TrackBar.Scroll += new System.EventHandler(this.TrackBar_Scroll);
+            // 
+            // CurrentTime
+            // 
+            this.CurrentTime.AutoSize = true;
+            this.CurrentTime.Location = new System.Drawing.Point(12, 421);
+            this.CurrentTime.Name = "CurrentTime";
+            this.CurrentTime.Size = new System.Drawing.Size(114, 17);
+            this.CurrentTime.TabIndex = 8;
+            this.CurrentTime.Text = "Текущее время:";
+            // 
+            // FullTime
+            // 
+            this.FullTime.AutoSize = true;
+            this.FullTime.Location = new System.Drawing.Point(369, 421);
+            this.FullTime.Name = "FullTime";
+            this.FullTime.Size = new System.Drawing.Size(102, 17);
+            this.FullTime.TabIndex = 9;
+            this.FullTime.Text = "Общее время:";
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(424, 12);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(86, 29);
+            this.Add.TabIndex = 10;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(424, 51);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(86, 29);
+            this.Delete.TabIndex = 11;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(424, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 17);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Громкость:";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.trackBar1.Location = new System.Drawing.Point(440, 154);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(4);
+            this.trackBar1.Maximum = 0;
+            this.trackBar1.Minimum = -100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(56, 157);
+            this.trackBar1.TabIndex = 20;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // MusicPlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 447);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.FullTime);
+            this.Controls.Add(this.CurrentTime);
+            this.Controls.Add(this.TrackBar);
+            this.Controls.Add(this.TrackName);
+            this.Controls.Add(this.Next);
+            this.Controls.Add(this.Previo);
+            this.Controls.Add(this.Continue);
+            this.Controls.Add(this.Pause);
+            this.Controls.Add(this.Play);
+            this.Controls.Add(this.PlayListBox);
+            this.MaximizeBox = false;
+            this.Name = "MusicPlayer";
+            this.Text = "MusicPlayer";
+            ((System.ComponentModel.ISupportInitialize)(this.TrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox PlayListBox;
+        private System.Windows.Forms.Button Play;
+        private System.Windows.Forms.Button Pause;
+        private System.Windows.Forms.Button Continue;
+        private System.Windows.Forms.Button Previo;
+        private System.Windows.Forms.Button Next;
+        private System.Windows.Forms.Label TrackName;
+        private System.Windows.Forms.TrackBar TrackBar;
+        private System.Windows.Forms.Label CurrentTime;
+        private System.Windows.Forms.Label FullTime;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ Music Player/MusicPlayer.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace Music_Player
+{
+    public partial class MusicPlayer : Form
+    {
+        private List<string> playList;
+        private WindowsMediaPlayer wmp;
+
+        public MusicPlayer()
+        {
+            InitializeComponent();
+            playList = new List<string>();
+            wmp = new WindowsMediaPlayer();
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Multiselect = true;
+            open.Filter = "MP3 files|*.mp3|WMV files|*.wmv";
+
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                for (int i = 0; i < open.FileNames.LongLength; i++)
+                {
+                    if (PlayListBox.Items.Contains(open.SafeFileNames.GetValue(i)) == false)
+                    {
+                        PlayListBox.Items.Add(open.SafeFileNames.GetValue(i));
+                        playList.Add(open.FileNames[i]);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Выбранный вами трек уже добавлен в плейлист!");
+                    }
+                }
+            }
+            if (PlayListBox.SelectedIndex < 0 & PlayListBox.Items.Count > 0) { PlayListBox.SetSelected(0, true); }
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < playList.Count; i++)
+            {
+                if (i == PlayListBox.SelectedIndex)
+                {
+                    CurrentTime.Text = "Текущее время:0:00:00";
+                    FullTime.Text = "Общее время:0:00:00";
+                    playList.RemoveAt(i);
+
+                    break;
+                }
+            }
+            if (PlayListBox.SelectedIndex != -1)
+            {
+                PlayListBox.Items.RemoveAt(PlayListBox.SelectedIndex);
+            }
+
+            wmp.controls.stop();
+            TrackBar.Enabled = false;
+            TrackName.Text = "";
+            TrackBar.Value = 0;
+            Continue.Enabled = false;
+        }
+
+        private void Play_Click(object sender, EventArgs e)
+        {
+            if (playList.Count > 0)
+            {
+                TrackBar.Enabled = true;
+                timer1.Enabled = true;
+                timer1.Interval = 1000;
+
+                PlayListBox.SetSelected(PlayListBox.SelectedIndex, true);
+                wmp.URL = playList[PlayListBox.SelectedIndex];
+
+                timer1.Start();
+                TrackName.Text = PlayListBox.SelectedItem.ToString();
+            }
+
+            Continue.Enabled = true;
+        }
+
+        private void Pause_Click(object sender, EventArgs e)
+        {
+            wmp.controls.pause();
+        }
+
+        private void Continue_Click(object sender, EventArgs e)
+        {
+            if (PlayListBox.SelectedIndex != -1)
+            {
+                wmp.controls.play();
+            }
+        }
+
+        private void Previo_Click(object sender, EventArgs e)
+        {
+            if (PlayListBox.Items.Count > 1 & PlayListBox.SelectedIndex != -1)
+            {
+
+                if (PlayListBox.SelectedIndex != 0)
+                {
+                    PlayListBox.SetSelected(PlayListBox.SelectedIndex - 1, true);
+                    wmp.URL = playList[PlayListBox.SelectedIndex];
+                    TrackName.Text = PlayListBox.SelectedItem.ToString();
+                }
+                else
+                {
+                    PlayListBox.SetSelected(PlayListBox.SelectedIndex + PlayListBox.Items.Count - 1, true);
+                    wmp.URL = playList[PlayListBox.SelectedIndex];
+                    TrackName.Text = PlayListBox.SelectedItem.ToString();
+                }
+            }
+        }
+
+        private void Next_Click(object sender, EventArgs e)
+        {
+            if (PlayListBox.Items.Count > 1 & PlayListBox.SelectedIndex != -1)
+            {
+
+                if (PlayListBox.SelectedIndex != PlayListBox.Items.Count - 1)
+                {
+                    PlayListBox.SetSelected(PlayListBox.SelectedIndex + 1, true);
+                    wmp.URL = playList[PlayListBox.SelectedIndex];
+                    TrackName.Text = PlayListBox.SelectedItem.ToString();
+                }
+                else
+                {
+                    PlayListBox.SetSelected(PlayListBox.SelectedIndex - PlayListBox.Items.Count + 1, true);
+                    wmp.URL = playList[PlayListBox.SelectedIndex];
+                    TrackName.Text = PlayListBox.SelectedItem.ToString();
+                }
+            }
+        }
+
+        private void TrackBar_Scroll(object sender, EventArgs e)
+        {
+            wmp.controls.currentPosition = TrackBar.Value;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TrackBar.Maximum = Convert.ToInt32(wmp.currentMedia.duration);
+            TrackBar.Value = Convert.ToInt32(wmp.controls.currentPosition);
+
+            if (wmp != null)
+            {
+                int s = (int)wmp.currentMedia.duration;
+                int h = s / 3600;
+                int m = (s - (h * 3600)) / 60;
+                s = s - (h * 3600 + m * 60);
+                FullTime.Text = String.Format("Общее время:{0:D}:{1:D2}:{2:D2}", h, m, s);
+
+                s = (int)wmp.controls.currentPosition;
+                h = s / 3600;
+                m = (s - (h * 3600)) / 60;
+                s = s - (h * 3600 + m * 60);
+                CurrentTime.Text = String.Format("Текущее время:{0:D}:{1:D2}:{2:D2}", h, m, s);
+            }
+            else
+            {
+                FullTime.Text = "Общее время:0:00:00";
+                CurrentTime.Text = "Текущее время:0:00:00";
+            }
+        }
+
+        private void PlayListBox_DoubleClick(object sender, EventArgs e)
+        {
+            if (playList.Count > 0 & PlayListBox.SelectedIndex != -1)
+            {
+                TrackBar.Enabled = true;
+                timer1.Enabled = true;
+                timer1.Interval = 1000;
+                wmp.URL = playList[PlayListBox.SelectedIndex];
+                timer1.Start();
+                TrackName.Text = PlayListBox.SelectedItem.ToString();
+                Continue.Enabled = true;
+            }
+        }
+
+        private void PlayListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (PlayListBox.SelectedIndex != -1)
+            {
+                Play.Enabled = true;
+            }
+            else
+            {
+                Play.Enabled = false;
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            try
+            {
+                trackBar1.Maximum = Convert.ToInt32(wmp.currentMedia.duration);
+                wmp.settings.volume = trackBar1.Value;
+            }
+            catch
+            {
+                // Если ни одно видео не запущено, игнорируем нажатие кнопки
+            }
+        }
+    }
+}
